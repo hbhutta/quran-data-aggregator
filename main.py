f4fe50bc9398546b295be1ff6746fe81a0b2bbad
Haad Bhutta <112280419+hbhutta@users.noreply.github.com>
2023-05-11 13:52:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,50 @@
 import requests
 
-quranenc_r = requests.get('https://quranenc.com/api/v1/translations/list')
-if quranenc_r.status_code == 200:
-   print(quranenc_r.json())
+"""
+Constraints:
+1. 0 < surah < 144
+2. 0 < aya < ...
+
+For the surah that is being accessed,
+we need to know the upper bound on the aya 
+so that this is not exceeded
+"""
+surah = 2
+verse = 1
+print(getText(surah,verse))
+
+
+api = f'https://quranenc.com/api/v1/translation/aya/english_saheeh/{surah}/{verse}'
+
+quranenc = requests.get(api)
+quranenc_json = ''
+
+if quranenc.status_code == 200:
+  quranenc_json = quranenc.json()
+
+
+
+'''
+Return: The specified surah verse
+Constraints: 1 <= surah <= 114 && 
+'''
+def getText(surah: int, verse: int) -> str:
+  text = ''
+  if verseInBounds(surah, verse):
+      text = surahJson["result"][verse]["arabic_text"]
+  return text
+
+def verseInBounds(surah: int, verse: int) -> bool:
+  if (1 <= surah and surah <= 114):
+    surahJson = quranenc.json()
+    surahVerseLimit = surahJson["result"]
+    if (1 <= verse and verse <= surahVerseLimit):
+      return True
+  else:
+    return False
+
+
+
+
+
+   
