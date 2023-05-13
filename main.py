fb58de1cbbfdf51efd066a814d2c168032fbf45e
Haad Bhutta <112280419+hbhutta@users.noreply.github.com>
2023-05-13 14:37:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,50 @@
 import requests
 
-sura = input("Enter sura #: ")
-aya = input("Enter aya #: ")
 
-apiForSura = f'https://quranenc.com/api/v1/translation/sura/english_saheeh/{sura}'
-apiForVerse = ''
 
-suraAPI = requests.get(apiForSura)
 
-if (1 <= sura && sura <= 114): # Sura number limit is 114
-  ayaLimit = -1
-  if (1 <= aya && aya <= ayaLimit):
-    api = f'https://quranenc.com/api/v1/translation/aya/english_saheeh/{sura}/{aya}'
-quranenc = requests.get(api)
-json = ''
-if quranenc.status_code == 200:
-  json = quranenc.json()
-  whatWeWant = json["result"]["arabic_text"]
-  print(whatWeWant)
-else:
-  print("-1")
-  quit()
+def getQuote(sura: int, aya: int, ayaRange: int) -> str:
+  # sura = int(input("Enter sura #: "))
+  # aya = int(input("Enter aya #: "))
+
+  apiLinkForSura = f'https://quranenc.com/api/v1/translation/sura/english_saheeh/{sura}'
+  apiLinkForAya = ''
+
+  suraAPI = requests.get(apiLinkForSura)
+  ayaAPI = ''
+
+  if (1 <= sura and sura <= 114): # Sura number limit is 114
+    suraJson = suraAPI.json()
+    ayaLimit = len(suraJson["result"])
+
+    if (range <= ayaLimit):
+      for ayaNumber in range(ayaRange):
+
+
+
+
+
+    # if (1 <= aya and aya <= ayaLimit): # Aya limit for that specific sura
+      apiLinkForAya = f'https://quranenc.com/api/v1/translation/aya/english_saheeh/{sura}/{aya}'
+      ayaAPI = requests.get(apiLinkForAya)
+      json = ayaAPI.json()
+      quote = json["result"]["arabic_text"]
+      print(quote)
+  else:
+    print(-1)
+    quit()
+  pass
+
+
+# quranenc = requests.get(api)
+# json = ''
+# if quranenc.status_code == 200:
+#   json = quranenc.json()
+#   whatWeWant = json["result"]["arabic_text"]
+#   print(whatWeWant)
+# else:
+#   print("-1")
+#   quit()
 
 # '''
 # Return: The specified surah verse
